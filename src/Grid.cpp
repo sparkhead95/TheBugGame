e45a3f8d777b635039fdb962e770d7fe24530f32
sparkhead95 <christianosholland@gmail.com>
2015-03-16 17:01:42 +0000

    attempting to recreate code. Use what is in "Write" and then delete it.

--- src/Grid.cpp
+++ src/Grid.cpp
@@ -23,67 +23,80 @@ Grid::Grid(int new_height, int new_length) {
 
 // might not even use this method
 void Grid::write() {
+	vector<string> row(this->length, "|-|");
+	vector<vector<string>> grid(this->height, row);
 
+	for (auto & e : aphidVector) {
+		pair<int, int> temp_pos = e.getPosition();
+		grid[temp_pos.first()][temp_pos.second()] = "|A|";
+	}
 }
 
 void Grid::create(vector<Aphid> aphidVector, vector<Ladybug> ladyVector) {
-	// ============= Aphid initial draw section ============================
-	vector<string> vertical(this->height);
-	vector<string> horizontal(this->length);
-	unsigned int j;
-	string aphid = "|A|";
-	string ladybug = "|L|";
-	string empty = "|-|";
 	for (vector<Aphid>::iterator aphIt = aphidVector.begin();
 			aphIt != aphidVector.end(); ++aphIt) {
 
 		pair<int, int> temp_pos = (*aphIt).getPosition();
-		for (j = 0; j < vertical.size(); j++) {
-			if (j == temp_pos.first) {
-				unsigned int i;
-				for (i = 0; i < horizontal.size(); i++)
-					if (i == temp_pos.second) {
-						horizontal.at(i) = aphid;
-					} else {
-						horizontal.at(i) = empty;
-					}
 
-				for (i = 0; i < horizontal.size(); i++)
-				 cout << " " << horizontal.at(i);
-				 cout << endl;
-				 }
-			}
-		}
-		// ========================================================
-
-		//  ============= ladybug initial draw section ==============
-
-		for (vector<Ladybug>::iterator ladIt = ladyVector.begin();
-				ladIt != ladyVector.end(); ++ladIt) {
-			pair<int, int> temp_pos = (*ladIt).getPosition();
-			for (j = 0; j < vertical.size(); j++) {
-				if (j == temp_pos.first) {
-					unsigned int i;
-					for (i = 0; i < horizontal.size(); i++)
-						if (i == temp_pos.second) {
-							horizontal.at(i) = ladybug;
-						} else {
-							if (horizontal.at(i).compare(aphid) != 0){
-								horizontal.at(i) = empty;
-							}
-						}
-
-					//vector<string> finalhorizontal(this->height);
-					//vector<string> lengthways(this->length);
-
-					for (i = 0; i < horizontal.size(); i++)
-						cout << " " << horizontal.at(i);
-					cout << endl;
-				}
-			}
-		}
 	}
-// ========================================================
+
+	/*// ============= Aphid initial draw section ============================
+	 vector<string> vertical(this->height);
+	 vector<string> horizontal(this->length);
+	 unsigned int j;
+	 string aphid = "|A|";
+	 string ladybug = "|L|";
+	 string empty = "|-|";
+	 for (vector<Aphid>::iterator aphIt = aphidVector.begin();
+	 aphIt != aphidVector.end(); ++aphIt) {
+
+	 pair<int, int> temp_pos = (*aphIt).getPosition();
+	 for (j = 0; j < vertical.size(); j++) {
+	 if (j == temp_pos.first) {
+	 unsigned int i;
+	 for (i = 0; i < horizontal.size(); i++)
+	 if (i == temp_pos.second) {
+	 horizontal.at(i) = aphid;
+	 } else {
+	 horizontal.at(i) = empty;
+	 }
+
+	 for (i = 0; i < horizontal.size(); i++)
+	 cout << " " << horizontal.at(i);
+	 cout << endl;
+	 }
+	 }
+	 }
+	 // ========================================================
+
+	 //  ============= ladybug initial draw section ==============
+
+	 for (vector<Ladybug>::iterator ladIt = ladyVector.begin();
+	 ladIt != ladyVector.end(); ++ladIt) {
+	 pair<int, int> temp_pos = (*ladIt).getPosition();
+	 for (j = 0; j < vertical.size(); j++) {
+	 if (j == temp_pos.first) {
+	 unsigned int i;
+	 for (i = 0; i < horizontal.size(); i++)
+	 if (i == temp_pos.second) {
+	 horizontal.at(i) = ladybug;
+	 } else {
+	 if (horizontal.at(i).compare(aphid) != 0) {
+	 horizontal.at(i) = empty;
+	 }
+	 }
+
+	 //vector<string> finalhorizontal(this->height);
+	 //vector<string> lengthways(this->length);
+	 for (unsigned int i = 0; i < horizontal.size(); i++)
+	 cout << " " << horizontal.at(i);
+	 cout << endl;
+
+	 }
+
+	 }
+	 }
+	 // ========================================================*/
 
 int Grid::getHeight() {
 	return this->height;
